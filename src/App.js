9a29fbec01b950d4b01e02db4298d7751e9267a5
paarth17032000 <paarth17032000@gmail.com>
2021-05-27 22:01:59 +0530

    store setup and reducers added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css'
 import {Provider}  from 'react-redux'
 import store from './store'
+import Preview from './components/Preview'
 
 function App() {
   return (
     <Provider store={store}>
-      abc
+      <Preview />
     </Provider>
   );
 }
